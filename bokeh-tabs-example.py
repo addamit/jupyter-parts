49a6eb9eb255504c53f379d7fc6c5e5cf8013287
addamit <47928681+addamit@users.noreply.github.com>
2020-04-21 16:37:18 -0400

    Update bokeh-tabs-example.py

--- bokeh-tabs-example.py
+++ bokeh-tabs-example.py
@@ -32,40 +32,68 @@ p2.hexbin(x,y, size=0.3, hover_alpha=0.5,line_color='grey')
 tab2 = Panel(child=p2, title="Hexbin Plot")
 p2.grid.visible = False
 
-mytabs=[ tab1, tab2 ]
+tabs=[ tab1, tab2 ]
 
 
-start, end = 1, 20
-samples_count = 5
-slider = Slider(start=start, end=end, value=start, step=1, title="Counts")
-select = Select(title="Count", value="aux", options=["box", "pack", "image", "user"])
+def modify_doc(doc, mytabs):
 
-renderer = hv.renderer('bokeh')##.instance(mode='server')
-hv.extension('bokeh')
-hv.output(size=200)
-links = pd.DataFrame(data['links'])
-print(links.head(3))
+	start, end = 1, 20
+	samples_count = 5
+	slider = Slider(start=start, end=end, value=start, step=1, title="Counts")
+	select = Select(title="Count", value="aux", options=["box", "pack", "image", "user"])
 
-def mychart(dummy_df):
-	print ("num: {}".format(len(dummy_df)))
+	renderer = hv.renderer('bokeh')##.instance(mode='server')
+	hv.extension('bokeh')
+	hv.output(size=200)
+	links = pd.DataFrame(data['links'])
+	print(links.head(3))
 	nodes = hv.Dataset(pd.DataFrame(data['nodes']), 'index')
-	chord = hv.Chord((links, nodes)).select(value=(5, None))
+	chord = hv.Chord((links, nodes)).select(value=(samples_count, None))
 	chord.opts(opts.Chord(cmap='Category20', edge_cmap='Category20', edge_color=dim('source').str(), labels='name', node_color=dim('index').str()))    	
 	# Create HoloViews plot and attach the document
-	hvplot = renderer.get_plot(chord)
-	return hvplot
+	hvplot = renderer.get_plot(chord, doc)
+	
+	def slider_update(attrname, old, new):
+		# Notify the HoloViews stream of the slider update 
+		print ("update received")
+		samples_count = new
+		
+		links = pd.DataFrame(data['links'])
+		print(links.head(3))
+		nodes = hv.Dataset(pd.DataFrame(data['nodes']), 'index')
+		chord = hv.Chord((links, nodes)).select(value=(samples_count, None))
+		chord.opts(opts.Chord(cmap='Category20', edge_cmap='Category20', edge_color=dim('source').str(), labels='name', node_color=dim('index').str()))    	
+		# Create HoloViews plot and attach the document
+		hvplot = renderer.get_plot(chord, doc)
+		tab3 = Panel(child=row(slider, hvplot.state), title="Chord Plot")
 
-dummy_df = pd.DataFrame({'x' : [1,2,3], 'y' : [4,5,6]})
-stream = hv.streams.Pipe(dummy_df=dummy_df)
-dmap1 = hv.DynamicMap(mychart, streams=[stream])
-# dmap1 = hv.DynamicMap(mychart, kdims='num').redim.range(num=(1,10))
-# widget0 = renderer.get_widget(dmap1, None, position='above')
-widget1 = renderer.get_plot(dmap1, None, position='above').state
+		mytabs.append(tab3)
+		views = Tabs(tabs = mytabs)
+		layout=row(views)
+		doc.add_root(layout)
+	
+		return doc
 
-tab3 = Panel(child=row( widget1), title="Chord Plot")
+	slider.on_change('value', slider_update)
 
-mytabs.append(tab3)
-views = Tabs(tabs = mytabs)
-layout=row(views)
-curdoc().add_root(layout)
+	
+	def select_update(attrname, old, new):
+		# Notify the HoloViews stream of the slider update 
+		print ("update received. Old: {} New: {}".format(old, new))
+		
+
+	select.on_change('value', select_update)		
+
+	# Combine the holoviews plot and widgets in a layout
+
+	tab3 = Panel(child=row(slider, hvplot.state), title="Chord Plot")
+
+	mytabs.append(tab3)
+	views = Tabs(tabs = mytabs)
+	layout=row(views)
+	doc.add_root(layout)
+	
+	return doc
+
+doc = modify_doc(curdoc(), tabs) 
 
